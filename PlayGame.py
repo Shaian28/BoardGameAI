f8a3674d455a29dfac6a7d3ebdc606a6aa448639
Shaian <s205080@student.dtu.dk>
2025-03-20 21:32:40 +0100

    Updated the state

--- PlayGame.py
+++ PlayGame.py
@@ -1,3 +1,4 @@
+import numpy as np
 import checkers as ch
 import Tree
 
@@ -38,45 +39,107 @@ def move(start, end):
         return False
 
 # Updating the pieces setup and role for the AI
-def update_state():
-    return 0
+def update_state(checker):
+    # Getting the coordinates
+    blackManX, blackManY = np.where(checker.board == 'b')
+    redManX, redManY = np.where(checker.board == 'r')
+    blackKingX, blackKingY = np.where(checker.board == 'B')
+    redKingX, redKingY = np.where(checker.board == 'R')
+
+    # Combining them together
+    blackMan = [(int(item1), int(item2)) for item1, item2 in zip(blackManY, blackManX)]#zip(blackManX, blackManY)]
+    redMan = [(int(item1), int(item2)) for item1, item2 in zip(redManY, redManX)]#zip(redManX, redManY)]
+    blackKing = [(int(item1), int(item2)) for item1, item2 in zip(blackKingY, blackKingX)]#zip(blackKingX, blackKingY)]
+    redKing = [(int(item1), int(item2)) for item1, item2 in zip(redKingY, redKingX)]#zip(redKingX, redKingY)]
+    
+    # Initialising the dictionaries
+    blackState = {}
+    redState = {}
+
+    # Giving each key the right value
+    for idx, place in enumerate(blackMan):
+        blackState[f"B{idx + 1}"] = [place, "Man"]
+        endIdx = idx
+    for idx, place in enumerate(blackKing):
+        blackState[f"B{idx + endIdx}"] = [place, "King"]
+    for idx, place in enumerate(redMan):
+        redState[f"R{idx + 1}"] = [place, "Man"]
+        endIdx = idx
+    for idx, place in enumerate(redKing):
+        redState[f"R{idx + endIdx}"] = [place, "King"]
+    
+    # Making the state
+    state = [blackState, redState]
+
+    # Returning the state
+    return state
 
 # Test to see the game has ended
-def terminal_test(killCondition, AIPieces, playerPieces):
-    if killCondition > 40 or AIPieces == 0 or playerPieces == 0:
+def terminal_test(killCondition, nodeState):
+    # Making a node out of the state
+    node = Tree.Node(nodeState)
+    
+    # All the pieces
+    AIPieces = len(list(node.state[0].keys()))
+    playerPieces = len(list(node.state[1].keys()))
+
+    # All the legal moves
+    node.legal_moves(True)
+    AILegalMove = len(node.legalMove)
+    node.legal_moves(False)
+    playerLegalMove = len(node.legalMove)
+
+    # The condition for terminal game
+    if killCondition > 40 or AIPieces == 0 or playerPieces == 0 or AILegalMove == 0 or playerLegalMove == 0:
+        if killCondition > 40 or AILegalMove == 0 or playerLegalMove == 0:
+            print("The game is a draw")
+        elif playerPieces == 0:
+            print("The AI won")
+        elif playerPieces == 0:
+            print("You won")
+        
+        # End the game
         return False
+    
+    # Continue the game
     return True
 
 # The board being played on
 board = ch.Checkers()
 board.print_board()
 
-# Initial state
-initial_state = [{"B1": [(1, 0), "Man"], "B2": [(3, 0), "Man"], "B3": [(5, 0), "Man"], "B4": [(7, 0), "Man"],
-                  "B5": [(0, 1), "Man"], "B6": [(2, 1), "Man"], "B7": [(4, 1), "Man"], "B8": [(6, 1), "Man"],
-                  "B9": [(1, 2), "Man"], "B10": [(3, 2), "Man"], "B11": [(5, 2), "Man"], "B12": [(7, 2), "Man"]},
-                 {"R1": [(0, 7), "Man"], "R2": [(2, 7), "Man"], "R3": [(4, 7), "Man"], "R4": [(6, 7), "Man"],
-                  "R5": [(1, 6), "Man"], "R6": [(3, 6), "Man"], "R7": [(5, 6), "Man"], "R8": [(7, 6), "Man"],
-                  "R9": [(0, 5), "Man"], "R10": [(2, 5), "Man"], "R11": [(4, 5), "Man"], "R12": [(6, 5), "Man"]}]
+# The state of the board
+state = update_state(board)
 
-# The game being played
+# Keeping track of the game
 turn = 0
 kill = 0
-while terminal_test(turn - kill):
+
+# The game being played
+while terminal_test(turn - kill, state):
     # Updating the turn
     turn += 1
 
     # The AI's turn
-    AIStart, AIEnd = AI_turn(initial_state if turn == 0 else update_state())
-    while not move(AIStart, AIEnd):
-        AIStart, AIEnd = AI_turn(initial_state if turn == 0 else update_state())
+    AIStart, AIEnd = AI_turn(state)
+    print((AIStart, AIEnd))
+    #while not move(AIStart, AIEnd):
+    while not move((AIStart[1], AIStart[0]), (AIEnd[1], AIEnd[0])):
+        AIStart, AIEnd = AI_turn(state)
+
+    if not terminal_test(turn - kill, state):
+        break
     
     # The player's turn
     playerStart, playerEnd = player_turn()
-    while not move(playerStart, playerEnd):
+    #while not move(playerStart, playerEnd):
+    while not move((playerStart[1], playerStart[0]), (playerEnd[1], playerEnd[0])):
         playerStart, playerEnd = player_turn()
     
     # Check if a kill happened this turn
-    if AIStart[0] - AIEnd[0] > 2 or AIStart[1] - AIEnd[1] > 2 or playerStart[0] - playerEnd[0] > 2 or playerStart[1] - playerEnd[1] > 2:
+    if abs(AIStart[0] - AIEnd[0]) > 1 or abs(AIStart[1] - AIEnd[1]) > 1 or abs(playerStart[0] - playerEnd[0]) > 1 or abs(playerStart[1] - playerEnd[1]) > 1:
         kill = turn
+    
+    # Updating the state
+    state = update_state(board)
     
